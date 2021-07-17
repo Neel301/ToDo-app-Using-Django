2eafbbf41f69758fbe856403f183851bcf262b32
Neel Patel <57136375+Neel301@users.noreply.github.com>
2021-07-17 16:46:48 +0200

    Update settings.py

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TodoDB',
         'USER': 'postgres',
-        'PASSWORD': 'Neel@1234.',
+        'PASSWORD': 'Password',
         'HOST': 'localhost'
     }
 }
